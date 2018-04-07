229a92d81a0a6f9a726c995d9e91ffbf0e0f4c3d
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2018-04-07 01:27:52 -0300

    Exercício 11 - FirebaseAuth
    Usar os recursos de autenticação do próprio firebase para segurança da aplicaçao

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthService implements CanActivate {
 
-  canActivate() {
+
+
+  canActivate(): boolean {
     console.log('AuthService#canActivate was called');
     return true;
   }
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
 }
